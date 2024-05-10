b9a7c13bef81b32d2df0ccc78994001c795ef805
Jakub Konka <kubkon@jakubkonka.com>
2024-05-10 11:42:09 +0200

    update to latest zig

--- build.zig
+++ build.zig
@@ -6,25 +6,27 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "ZigKit",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .optimize = mode,
         .target = target,
     });
     b.installArtifact(lib);
 
     _ = b.addModule("ZigKit", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .optimize = mode,
-        .target = target,
-    });
-    main_tests.linkFramework("CoreFoundation");
-    main_tests.linkFramework("Security");
-    main_tests.addFrameworkPath(.{ .path = "/System/Library/Frameworks" });
+    if (std.zig.system.darwin.getSdk(b.allocator, target.result)) |sdk| {
+        const main_tests = b.addTest(.{
+            .root_source_file = b.path("src/main.zig"),
+            .optimize = mode,
+            .target = target,
+        });
+        main_tests.linkFramework("CoreFoundation");
+        main_tests.linkFramework("Security");
+        main_tests.addSystemFrameworkPath(.{ .cwd_relative = b.pathJoin(&.{ sdk, "/System/Library/Frameworks" }) });
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&b.addRunArtifact(main_tests).step);
+        const test_step = b.step("test", "Run library tests");
+        test_step.dependOn(&b.addRunArtifact(main_tests).step);
+    }
 }
